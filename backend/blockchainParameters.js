6fce0aad34fee6d4c4a54aa34a1c9bab0bca6197
ethanJlab <ethanjanicki89@gmail.com>
2023-08-05 17:35:44 -0400

    feat: add 2 testnets

---
+++ backend/blockchainParameters.js
@@ -0,0 +1,12 @@
+// this file will serve as a place to store all the blockchain parameters that will be used in the api calls
+
+// Ethereum testnet data (Goerli Testnet)
+
+const testEthID = 5;
+const testEthRPC = "https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161"
+const testEthRPCList = ["https://eth-goerli.public.blastapi.io/","https://rpc.ankr.com/eth_goerli"]
+
+// Polygon (Matic) testnet data (Mumbai Testnet)
+
+const testMaticID = 13881;
+const testMaticRPC = "https://rpc-mumbai.maticvigil.com/"
